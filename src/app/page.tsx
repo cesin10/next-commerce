32b814a47a6dd6945232e09ad5129b5ac23adf74
Cesar J Frietas <teste@teste.com>
2023-12-17 14:37:35 -0300

    Listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Next E-commerce 14</h1>
+    <div className="max-w-7 mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">Prod1</div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">Prod1</div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">Prod1</div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">Prod1</div>
+
+      </div>     
     </div>
   )
 }
