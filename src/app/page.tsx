4023533ccab7ec2081c9b7833147f209d93a403c
Cesar J Frietas <teste@teste.com>
2023-12-17 20:25:32 -0300

    Definindo tipagem pra produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
-import Image from 'next/image'
+import { Producttype } from "@/types/ProductType"
 
-export default function Home() {
+
+async function getData() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
+export default async  function Home() {
+  const products = await getData()
+  console.log(products)
   return (
     <div className="max-w-7 mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">Prod1</div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">Prod1</div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">Prod1</div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">Prod1</div>
-
+        |{products.map((p: Producttype) => <div key={p.id}>{p.title}</div>)}
       </div>     
     </div>
   )
---
+++ src/types/ProductType.ts
@@ -0,0 +1,9 @@
+export type Producttype ={
+    id: number; 
+    title: string;
+    price: number | null; 
+    description: string | null; 
+    image: string; 
+    category: string;
+
+}
